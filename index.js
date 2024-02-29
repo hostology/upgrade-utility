76e2fdafcafa2cea6264df3fcd3a3c9b451e6f3a
Mark Somerfield <somerfield.md@gmail.com>
2024-02-29 12:03:14 +0000

    Can pull matching import references from packages

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-const path = require('path');
+const run = require('./app');
+
+const rootPath = `C:\\hostology\\github\\hostology-admin-web`
+const packages = 'packages';
+const portals = 'portals';
+
+const getPath = (path) => {
+  return `${rootPath}\\${path}`;
+};
+
+const targets = {
+  targetDir: getPath(`${portals}\\admin`),
+  exclude: [/.*\\node_modules\\.*/, /.*\\public\\.*/, /.*\\\.next\\.*/],
+  fileTypes: ['js', 'jsx', 'ts', 'tsx']
+}
+
+const modules = [{ 
+  id: '@hostology/ui',
+  directory: getPath(`${packages}\\ui`)
+}, { 
+  id: '@hostology/components',
+  directory: getPath(`${packages}\\components`)
+}]
+
+run({ targets, modules });
+
+
+
+
+
+/* const path = require('path');
+const jscodeshift = require('jscodeshift');
+
 const Runner = require('jscodeshift/src/Runner');
 
+
+
 // just the paths of the files to apply the transformation
-const paths = ['C:\\hostology\\github\\hostology-admin-web\\portals\\admin\\pages\\users'];
+// const paths = ['C:\\hostology\\github\\hostology-admin-web\\portals\\admin\\pages\\users'];
+
 
 /**
  * taken from
  * @link https://github.com/facebook/jscodeshift/blob/48f5d6d6e5e769639b958f1a955c83c68157a5fa/bin/jscodeshift.js#L18
  */
+/*
 const options = {
   transform: 'transforms\\list-imports.js',
   verbose: 0,
@@ -22,14 +58,16 @@ const options = {
   parser: 'babel',
   stdin: false
 }
-
+*/
 
 /**
  * taken from
  * @link https://github.com/facebook/jscodeshift/blob/48f5d6d6e5e769639b958f1a955c83c68157a5fa/bin/jscodeshift.js#L135
  */
+/*
 Runner.run(
   /^https?/.test(options.transform) ? options.transform : path.resolve(options.transform),
   paths,
   options
 );
+*/
