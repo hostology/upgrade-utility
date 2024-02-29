4d64ed24e6f484516b5be25600e8b5857748dd68
Mark Somerfield <somerfield.md@gmail.com>
2024-02-29 16:51:10 +0000

    Some code refactoring

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const fileUtils = require("./utils/file-utils");
+const fileProvider = require('./utils/file-provider');
+const transformUtils = require("./utils/transform-utils");
+const moduleReferenceBuilder = require("./utils/module-reference-builder");
+const transformBuilder = require("./utils/transform-builder");
 
 function run({ targets, modules }) {
-  const files = fileUtils.getAllFilesRecursively(targets);
-  const referencedFiles = fileUtils.getAllReferencedFiles({ modules, files });
-  const filesToMove = fileUtils.defineTransforms({ modules, files: referencedFiles }).filter(f => f.moveFile);
+  const files = fileProvider.getAllFilesRecursively(targets);
+  const referencedFiles = moduleReferenceBuilder.getAllReferencedFiles({ modules, files });
+  const filesToMove = transformBuilder.defineTransforms({ modules, files: referencedFiles }).filter(f => f.moveFile);
+  transformUtils.doTransforms(filesToMove);
+  /*
   filesToMove.forEach(element => {
     console.log(`${element.file} => ${element.target}`)
-  });
+  });*/
 }
 
 module.exports = run;
--- utils/file-utils.js
+++ utils/file-utils.js
@@ -2,13 +2,6 @@ const fs = require('fs');
 const path = require('path')
 const esprima = require('esprima-next');
 
-function _tryResolve(path) {
-  return {
-    exists: fs.existsSync(path) && !fs.lstatSync(path).isDirectory(),
-    path
-  }
-}
-
 const resolvers = [
   (path) => _tryResolve(path) ,
   (path) => _tryResolve(`${path}.js`),
@@ -21,74 +14,7 @@ const resolvers = [
   (path) => _tryResolve(`${path}/index.tsx`),
 ]
 
-const fileTypes = ['js', 'jsx', 'ts', 'tsx'];
-
-function getAllFilesRecursively({ targetDir, exclude }) {
-  const files = fs.readdirSync(targetDir);
-  
-  if (_fileIsExcluded({ file: targetDir, exclude })) {
-    return [];
-  }
-
-  return files.reduce((acc, file) => {
-    const filePath = path.join(targetDir, file);
-    const stat = fs.lstatSync(filePath);
-    if (stat.isDirectory()) {
-      return [...acc, ...getAllFilesRecursively({ targetDir: filePath, exclude })]
-    } else if (_fileIsOfType(file, fileTypes)) {
-      return [...acc, filePath];
-    }
-    return acc;
-  }, []);
-}
-
-function getAllReferencedFiles({ modules, files }) {
-  return files.reduce((acc, file) => {
-    return [...acc, ..._getReferencedFilesRecursively({ modules, file })]
-  }, [])
-};
-
-function defineTransforms({ modules, files }) {
-  return files.map(({ file }) => {
-    const module = modules.find(module => file.startsWith(module.directory));
-    if (!module) return { file, moveFile: false };
-    const target = file.replace(module.directory, `${module.target}`);
-    return { file, target, moveFile: true };
-  });
-}
-
-function _fileIsOfType(file) {
-  return fileTypes.some(fileType => file.endsWith(fileType));
-}
-
-function _getReferencedFilesRecursively({ modules, file, ref }) {
-  if (!_fileIsOfType(file, fileTypes)) return [{ file, ref }];
-
-  const source = fs.readFileSync(file, "utf8");
-  const result = esprima.parseModule(source, { jsx: true });
-  const importDeclarations = result.body
-    .filter(b => b.type === 'ImportDeclaration')
-    .map(b => b.source.value)
-    .filter(_isMatchOrLocal(modules));
-  
-  if (importDeclarations.length === 0) return [{ file, ref }];
-
-  return importDeclarations.map(ref => _getReferencedFilesRecursively({
-    modules,
-    file: _getModulePath(modules, ref, file),
-    ref
-  })).flat();
-};
-
-function _getModulePath(modules, ref, file) {
-  const module = modules.find(module => ref.startsWith(module.id));
-    const filePath = module 
-      ? `${module.directory}${ref.replace(module.id, '')}`.replaceAll('\/', '\\')
-      : path.resolve(path.dirname(file), ref);
-    return _resolve(filePath);
-}
-
-function _resolve(path) {
+function tryResolveReference(path) {
   for (var index = 0; index < resolvers.length; index++) {
     const resolver = resolvers[index];
     const resolved = resolver(path);
@@ -100,27 +26,13 @@ function _resolve(path) {
   return undefined;
 }
 
-function _fileIsExcluded({ file, exclude }) {
-  if (!exclude) return false;
-  return exclude.some(regex=> file.match(regex));
-}
-
-function _isMatchOrLocal(modules) {
-  return (importPath) => {
-    return _doesMatchTargetModules(modules, importPath) || _isLocalImport(importPath);
+function _tryResolve(path) {
+  return {
+    exists: fs.existsSync(path) && !fs.lstatSync(path).isDirectory(),
+    path
   }
 }
 
-function _doesMatchTargetModules(targetModules, importPath) {
-  return targetModules.some(module => importPath.startsWith(module.id));
-}
-
-function _isLocalImport(importPath) {
-  return importPath.startsWith('.');
-}
-
 module.exports = {
-  getAllFilesRecursively,
-  getAllReferencedFiles,
-  defineTransforms,
+  tryResolveReference,
 }
