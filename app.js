32e757c500258896d2ef0add4e21a62168ad056e
Nui Thiwakorn <thiwakorn@mohara.co>
2024-03-04 11:00:08 +0700

    Auto-format app.js and module-reference-builder.js

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const fileProvider = require('./utils/file-provider');
+const fileProvider = require("./utils/file-provider");
 const transformUtils = require("./utils/transform-utils");
 const moduleReferenceBuilder = require("./utils/module-reference-builder");
 const transformBuilder = require("./utils/transform-builder");
 
 function run({ targets, modules }) {
   const files = fileProvider.getAllFilesRecursively(targets);
-  const referencedFiles = moduleReferenceBuilder.getAllReferencedFiles({ modules, files });
-  const filesToMove = transformBuilder.defineTransforms({ modules, files: referencedFiles }).filter(f => f.moveFile);
+  const referencedFiles = moduleReferenceBuilder.getAllReferencedFiles({
+    modules,
+    files,
+  });
+  const filesToMove = transformBuilder
+    .defineTransforms({ modules, files: referencedFiles })
+    .filter((f) => f.moveFile);
   transformUtils.doTransforms(filesToMove);
   
   /*
--- utils/module-reference-builder.js
+++ utils/module-reference-builder.js
@@ -1,24 +1,33 @@
-const fs = require('fs');
-const path = require('path')
-const esprima = require('esprima-next');
-const fileUtils = require('./file-utils');
-const { supportedJsFileTypes } = require('./constants');
+const fs = require("fs");
+const path = require("path");
+const esprima = require("esprima-next");
+const fileUtils = require("./file-utils");
+const { supportedJsFileTypes } = require("./constants");
 
 function getAllReferencedFiles({ modules, files }) {
   const allReferences = files.reduce((acc, file) => {
-    return [...acc, ..._getReferencedFilesRecursively({ modules, file })]
-  }, [])
+    return [...acc, ..._getReferencedFilesRecursively({ modules, file })];
+  }, []);
 
-  const reducedReferences = allReferences.reduce((acc, ref) => { 
-    const existingRef = acc.find(a => a.file === ref.file) ?? { file: ref.file, references: [] };
-    return [...acc.filter(a => a.file !== ref.file), {
+  const reducedReferences = allReferences.reduce((acc, ref) => {
+    const existingRef = acc.find((a) => a.file === ref.file) ?? {
       file: ref.file,
-      references: [...existingRef.references, { ref: ref.ref, from: ref.from }]
-    }]
+      references: [],
+    };
+    return [
+      ...acc.filter((a) => a.file !== ref.file),
+      {
+        file: ref.file,
+        references: [
+          ...existingRef.references,
+          { ref: ref.ref, from: ref.from },
+        ],
+      },
+    ];
   }, []);
 
   return reducedReferences;
-};
+}
 
 function _getReferencedFilesRecursively({ modules, file, ref, from }) {
   if (!_fileIsOfType(file)) return [{ file, ref, from }];
@@ -26,47 +35,53 @@ function _getReferencedFilesRecursively({ modules, file, ref, from }) {
   const source = fs.readFileSync(file, "utf8");
   const result = esprima.parseModule(source, { jsx: true });
   const importDeclarations = result.body
-    .filter(b => b.type === 'ImportDeclaration')
-    .map(b => b.source.value)
+    .filter((b) => b.type === "ImportDeclaration")
+    .map((b) => b.source.value)
     .filter(_isMatchOrLocal(modules));
-  
+
   return [
     { file, ref, from },
-    ...importDeclarations.map(ref => _getReferencedFilesRecursively({
-      modules,
-      file: _getModulePath(modules, ref, file),
-      ref,
-      from: file
-    })).flat()
+    ...importDeclarations
+      .map((ref) =>
+        _getReferencedFilesRecursively({
+          modules,
+          file: _getModulePath(modules, ref, file),
+          ref,
+          from: file,
+        })
+      )
+      .flat(),
   ];
-};
+}
 
 function _fileIsOfType(file) {
-  return supportedJsFileTypes.some(fileType => file.endsWith(fileType));
+  return supportedJsFileTypes.some((fileType) => file.endsWith(fileType));
 }
 
 function _getModulePath(modules, ref, file) {
-  const module = modules.find(module => ref.startsWith(module.id));
-    const filePath = module 
-      ? `${module.directory}${ref.replace(module.id, '')}`.replaceAll('\/', '\\')
-      : path.resolve(path.dirname(file), ref);
-    return fileUtils.tryResolveReference(filePath);
+  const module = modules.find((module) => ref.startsWith(module.id));
+  const filePath = module
+    ? `${module.directory}${ref.replace(module.id, "")}`.replaceAll("/", "\\")
+    : path.resolve(path.dirname(file), ref);
+  return fileUtils.tryResolveReference(filePath);
 }
 
 function _isMatchOrLocal(modules) {
   return (importPath) => {
-    return _doesMatchTargetModules(modules, importPath) || _isLocalImport(importPath);
-  }
+    return (
+      _doesMatchTargetModules(modules, importPath) || _isLocalImport(importPath)
+    );
+  };
 }
 
 function _doesMatchTargetModules(targetModules, importPath) {
-  return targetModules.some(module => importPath.startsWith(module.id));
+  return targetModules.some((module) => importPath.startsWith(module.id));
 }
 
 function _isLocalImport(importPath) {
-  return importPath.startsWith('.');
+  return importPath.startsWith(".");
 }
 
 module.exports = {
   getAllReferencedFiles,
-}
+};
